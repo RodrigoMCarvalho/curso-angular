cd5ce5da05352f89bfc54976b3d1a2473100bd27
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-08-26 19:25:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primeiro-projeto';
+  title = 'Curso Angular';
+  teste = 'Teste';
 }
